5d2fad5421d25c1f721461e30e438f222fdd40ef
sivan wolberg <sivan@growthspace.us>
2020-09-13 23:48:41 +0300

    clean up

--- functions/src/routes/index.ts
+++ functions/src/routes/index.ts
@@ -1,9 +1,7 @@
 import { PingController } from "./Ping.route";
-import { TestController } from "./Test.route";
 import { EntityController } from "./Entity.route";
 
 export const apiRoutes = [
     PingController,
-    TestController,
     EntityController,
 ]
