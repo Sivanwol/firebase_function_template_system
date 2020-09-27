71809c4454f0a8564c8d71a0ff2f9c6fc21c691a
sivan wolberg <sivan@growthspace.us>
2020-09-27 16:04:26 +0300

    well it building now so it best

--- functions/src/common/firebase.ts
+++ functions/src/common/firebase.ts
@@ -1,12 +1,18 @@
 
 import * as firebaseEmulator from '@firebase/testing';
 import * as firebase from "firebase-admin";
+import * as serviceAccount  from "../serviceAccountKey.json";
+// import { logger } from 'firebase-functions';
 const projectId = 'ictravel-dev-planning';
 export class FirebaseHandler {
     public static db: firebase.firestore.Firestore | firebaseEmulator.firestore.Firestore;
 
     public static setupFirebase(firebase: any) {
-        firebase.initializeApp({ credential: firebase.credential.applicationDefault() });
+        const adminConfig = JSON.parse(process.env.FIREBASE_CONFIG);
+        adminConfig.credential = firebase.credential.cert(serviceAccount);
+        console.log(adminConfig);
+        firebase.initializeApp(adminConfig);
+        // firebase.initializeApp({ credential: firebase.credential.applicationDefault() });
         firebase.firestore().settings({ timestampsInSnapshots: true });
     }
     public static setupFirebaseTest(mockFirebase: any) {
